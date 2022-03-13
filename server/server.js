e865b2e8b5dc7ae7feccd68fcf4270fcefa35a19
Vetle de Vries <vetle1608@gmail.com>
2022-03-13 19:13:41 +0100

    get /api/questions/

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import * as path from 'path'
+import { getRandomQuestion } from './questions.js'
 
 const app = express()
 
 app.use(bodyParser.json())
+
+app.get('/api/questions', (req, res) => {
+    const { id, category, question, answers } = getRandomQuestion()
+    res.json({ id, category, question, answers })
+  })
 
 app.use(express.static('../client/dist'))
 app.use((req, res, next) => {
