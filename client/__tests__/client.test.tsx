c8ab0eddd26d5d333ed70f0a6af6a62d6ea7b94d
Vetle de Vries <vetle1608@gmail.com>
2022-03-14 19:05:08 +0100

    wip testing with react-query

--- client/__tests__/client.test.tsx
+++ client/__tests__/client.test.tsx
@@ -1,7 +1,7 @@
 import pretty from 'pretty'
 import React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider, setLogger } from 'react-query'
 import { MemoryRouter } from 'react-router-dom'
 import { Home } from '../src/pages/Home'
 import { RandomQuestion } from '../src/pages/RandomQuestion'
@@ -14,7 +14,31 @@ describe('quiz', () => {
         refetchOnWindowFocus: false,
         refetchOnMount: true,
         keepPreviousData: true,
+        retry: false,
       },
+    },
+  })
+  const wrapper = ({ children }: Element) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+  setLogger({
+    log: console.log,
+    warn: console.warn,
+    error: () => {},
+  })
+  const question = () => ({
+    id: 69,
+    question: "Good day sir what's your name sir",
+    answer: {
+      answer_a: 'My name deez',
+      answer_b: 'Mike Oxlong',
+      answer_c:
+        'I am under the water, please help me, there is too much raining, burururuurur',
+    },
+    correct_answers: {
+      answer_a_correct: 'true',
+      answer_b_correct: 'false',
+      answer_c_correct: 'false',
     },
   })
 
@@ -36,6 +60,7 @@ describe('quiz', () => {
     )
     expect(pretty(container.innerHTML)).toMatchSnapshot()
   })
+
   it('renders RandomQuestion component', () => {
     render(
       <QueryClientProvider client={queryClient}>
@@ -47,4 +72,16 @@ describe('quiz', () => {
     )
     expect(pretty(container.innerHTML)).toMatchSnapshot()
   })
+
+  // it('fetches question', () => {
+  //   render(
+  //     <QueryClientProvider client={queryClient}>
+  //       <MemoryRouter initialEntries={['/questions']}>
+  //         <RandomQuestion />
+  //       </MemoryRouter>
+  //     </QueryClientProvider>,
+  //     container
+  //   )
+  //   expect(pretty(container.innerHTML)).toMatchSnapshot()
+  // })
 })
