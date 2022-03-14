dd22843452e7998d1e20ee19d45aec620259ad21
Vetle de Vries <vetle1608@gmail.com>
2022-03-14 17:40:37 +0100

    more server tests

--- client/src/interfaces/fetch.ts
+++ client/src/interfaces/fetch.ts
@@ -1,16 +1,23 @@
 export interface TQuestions {
   id: number
   question: string
-  description?: string | null
+  category?: string
   answers: Record<string, string | null>
+  description?: string | null
   multiple_correct_answers?: 'true' | 'false'
   correct_answers: Record<string, 'true' | 'false'>
   correct_answer: string
   explanation?: string | null
   tip?: string | null
   tags?: Tag[]
-  category?: string
   difficulty?: string
+}
+
+export interface TReqProps {
+  id: number
+  question: string
+  category?: string
+  answers: Record<string, string | null>
 }
 
 export interface Tag {
