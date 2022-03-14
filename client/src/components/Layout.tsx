276dc771184d063307c9eec55a045f0dd75ade0b
Magnus Soleim <magnus@soleim.com>
2022-03-14 02:56:42 +0100

    tailwind footer

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,10 +1,16 @@
 import { Nav } from './Nav'
-
+import { Footer } from './Footer'
 export const Layout: React.FC = ({ children }) => {
   return (
     <>
-      <Nav />
-      <main className='p-6'>{children}</main>
+        <div className='flex justify-between flex-col min-h-screen'>
+      <div>
+        <Nav />
+        <main className='p-6'>{children}</main>
+      </div>
+      <Footer />
+    </div>
+      
     </>
   )
 }
