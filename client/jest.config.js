20111c87cad29a6b90ed2f98dfe818bbfea38996
Vetle de Vries <vetle1608@gmail.com>
2022-03-14 03:40:33 +0100

    convert to ts-jest

---
+++ client/jest.config.js
@@ -0,0 +1,17 @@
+// jest.config.js
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  collectCoverageFrom: [
+    '**/*.{jsx,tsx,ts,js}',
+    '!dist/**',
+    '!babel.config.js',
+    '!index.tsx',
+    '!interfaces/',
+    '!postcss.config.js',
+    '!tailwind.config.js',
+    '!jest.config.js',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
---
+++ jest.config.js
@@ -0,0 +1,21 @@
+// jest.config.js
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  collectCoverageFrom: [
+    '**/*.{jsx,tsx,ts,js}',
+    '!server/src/server.ts',
+    '!client/dist/**',
+    '!dist/**',
+    '!client/src/index.tsx',
+    '!client/coverage/**',
+    '!server/coverage/**',
+    '!coverage/**',
+    '!server/**.config.js',
+    '!client/**.config.js',
+    '!**.config.js',
+  ],
+  testMatch: ['**/__tests__/**.test.{jsx,tsx,ts,js}'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
