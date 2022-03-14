d5e512e557185e78e701826a4062107f165b3953
Vetle de Vries <vetle1608@gmail.com>
2022-03-14 21:18:31 +0100

    layout test

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -2,15 +2,16 @@ import { Nav } from './Nav'
 import { Footer } from './Footer'
 export const Layout: React.FC = ({ children }) => {
   return (
-    <>
-      <div className='flex justify-center bg-primary'>
-        <div className='flex justify-between flex-col min-h-screen w-3/5 bg-primary shadow-2xl'>
-          <Nav />
-          <main className='p-6'>{children}</main>
-
-          <Footer />
+    <div className='flex justify-between flex-col min-h-screen'>
+      <div className='flex flex-col min-w-full justify-center items-center'>
+        <Nav />
+        <div className='p-4 min-w-full flex justify-center items-center'>
+          <main className='p-6 flex max-w-5xl w-full md:w-4/5 lg:w-full bg-red-300'>
+            {children}
+          </main>
         </div>
       </div>
-    </>
+      <Footer />
+    </div>
   )
 }
